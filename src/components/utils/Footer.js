4437f14d39ef97c4d4989b96844baaf3aff5f716
Daniel Cermann <daniel.cermann@gmx.de>
2023-06-18 20:16:54 -0300

    moved styles to proper files

--- src/components/utils/Footer.js
+++ src/components/utils/Footer.js
@@ -1,38 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
+import './Footer.scss'
 
-class Footer extends Component {
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function (network) {
-        return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
-    }
-
-    return (
-      <footer>
-        <div className="col-md-12">
-          <div className="social-links">{networks}</div>
-
-          <div className="copyright py-4 text-center">
-            <div className="container">
-              <small>
-                Copyright &copy;{" "}
-                {this.props.sharedBasicInfo
-                  ? this.props.sharedBasicInfo.name
-                  : "???"}
-              </small>
-            </div>
-          </div>
-        </div>
-      </footer>
+function Footer(props) {
+  let networks = []
+  if (props.sharedBasicInfo) {
+    networks = props.sharedBasicInfo.social.map((network) =>
+      <span key={network.name} className="m-4">
+        <a href={network.url} target="_blank" rel="noopener noreferrer">
+          <i className={network.class}></i>
+        </a>
+      </span>
     );
   }
+
+  return (
+    <footer>
+      <div className="col-md-12">
+        <div className="social-links">{networks}</div>
+        <div className="copyright py-2 text-center">
+          <div className="container">
+            <small>
+              Copyright &copy;{" "}
+              {props.sharedBasicInfo
+                ? props.sharedBasicInfo.name
+                : "???"}
+            </small>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- src/components/utils/Sidebar.js
+++ src/components/utils/Sidebar.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Sidebar.scss'
 import { HashLink } from 'react-router-hash-link'
 
 function Sidebar(props) {
+	const [activeSection, setActiveSection] = useState('home');
+	const sectionRefs = useRef([]);
+
+	useEffect(() => {
+		document.querySelectorAll("section").forEach(e => {
+			sectionRefs.current.push(e);
+		});
+
+
+		// set observer that checks what section is active
+		window.addEventListener('scroll', event => {
+			sectionRefs.current.forEach(e => {
+				const dims = e.getBoundingClientRect()
+				if (Math.abs(dims.top) <= window.innerHeight * 0.5 || Math.abs(dims.bottom - window.innerHeight) <= window.innerHeight * 0.2) {
+					let t = e.attributes.id.value;
+					if (t !== activeSection) setActiveSection(t);
+				}
+			})
+		});
+	}, [])
+
+
+
 	return (
 		<div className='sidebar'>
 			<HashLink to='/#home'>
-				<img className='logo' src='dc-logo.svg' alt='DANIEL'/>
+				<img className='logo' src='dc-logo.svg' alt='DANIEL' />
 			</HashLink>
 			<div>
 				{
 					props.sections.map((section, id) =>
-						<HashLink to={`#${section}`} key={id} className={`section-label ${section === props.activeSection && 'active'}`}>
+						<HashLink to={`#${section}`} key={id} className={`section-label ${section === activeSection && 'active'}`}>
 							{section}
 						</HashLink>
 					)
@@ -21,4 +44,4 @@ function Sidebar(props) {
 	)
 }
 
-export default Sidebar
+export default Sidebar;
