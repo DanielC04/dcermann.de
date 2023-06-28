1e79a406d78d2b3d0463dbdd18ff1f67666bc5e5
Daniel Cermann <daniel.cermann@gmx.de>
2023-06-27 22:56:11 -0300

    added feedback after contact option

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,30 +1,41 @@
 import "./Contact.scss";
 import { BasicInfo } from "../loaded_data_types";
-import React, { useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 import { PUBLIC_KEY, SERVICE_ID, TEMPLATE_ID } from "./EmailjsLogin";
+import { Alert } from "react-bootstrap";
 
 interface Props {
     basicInfo: BasicInfo | undefined;
 }
 
+const ALERT_FADE_TIME_OUT = 5_000;
+
 function Contact(props: Props) {
+    const [alert, setAlert]: any = useState(undefined);
+
     const title = props.basicInfo?.section_name.contact;
     const form: any = useRef();
 
-    
     const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
-        console.log(e)
+        console.log(e);
         e.preventDefault();
-    
-        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
-          .then((result) => {
-              console.log(result.text);
-          }, (error) => {
-              console.log(error.text);
-          });
-      };
 
+        emailjs
+            .sendForm(SERVICE_ID, TEMPLATE_ID, form.current, PUBLIC_KEY)
+            .then(
+                (result) => {
+                    const successAlert = <CustomAlert isError={false} />;
+                    setAlert(successAlert);
+                    setTimeout(() => setAlert(undefined), ALERT_FADE_TIME_OUT);
+                },
+                (error) => {
+                    const errorAlert = <CustomAlert isError={false} />;
+                    setAlert(errorAlert);
+                    setTimeout(() => setAlert(undefined), ALERT_FADE_TIME_OUT);
+                }
+            );
+    };
 
     return (
         <section id="contact">
@@ -35,16 +46,28 @@ function Contact(props: Props) {
                         <div className="login-box">
                             <form ref={form} onSubmit={sendEmail}>
                                 <div className="user-box">
-                                    <input type="text" name="name" className="input" required />
+                                    <input
+                                        type="text"
+                                        name="name"
+                                        className="input"
+                                        required
+                                    />
                                     <label>Name</label>
                                 </div>
                                 <div className="user-box">
-                                    <input type="textx" name="email" className="input" required />
+                                    <input
+                                        type="textx"
+                                        name="email"
+                                        className="input"
+                                        required
+                                    />
                                     <label>Email</label>
                                 </div>
                                 <div className="user-box">
-                                    <textarea className="input" name="message">
-                                    </textarea>
+                                    <textarea
+                                        className="input"
+                                        name="message"
+                                    ></textarea>
                                     <label>Message</label>
                                 </div>
                                 <center>
@@ -52,11 +75,16 @@ function Contact(props: Props) {
                                         <i
                                             className={`devicon-line-plain icon`}
                                         ></i>
-                                        &gt; <span style={{marginLeft: '-12px'}}>_</span> SEND
+                                        &gt;{" "}
+                                        <span style={{ marginLeft: "-12px" }}>
+                                            _
+                                        </span>{" "}
+                                        SEND
                                         <span className="animation"></span>
                                     </button>
                                 </center>
                             </form>
+                            <div className="mt-4">{alert}</div>
                         </div>
                     </div>
                     <div className="d-sm-none d-xs-none d-md-flex col-md-4 col-lg-7 justify-content-center align-items-center">
@@ -65,6 +93,16 @@ function Contact(props: Props) {
                 </div>
             </div>
         </section>
+    );
+}
+
+function CustomAlert(props: { isError: boolean }) {
+    return (
+        <Alert variant={props.isError ? "danger" : "success"}>
+            {props.isError
+                ? "Sending Message failed :( Try again later"
+                : "Message sent successfully :)"}
+        </Alert>
     );
 }
 
