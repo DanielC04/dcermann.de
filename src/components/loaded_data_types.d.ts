1068eb2f35553383540ab4e73932b8202149d32a
Daniel <daniel@fedora.fritz.box>
2024-03-29 14:34:24 +0100

    installed vite dependency

--- src/components/loaded_data_types.d.ts
+++ src/components/loaded_data_types.d.ts
@@ -1,82 +1,74 @@
 export interface SharedData {
-    basic_info: BasicSharedInfo;
-    skills:     Skills;
+  basic_info: BasicSharedInfo;
+  skills: Skills;
 }
 
 export interface BasicSharedInfo {
-    name:   string;
-    titles: string[];
-    social: Social[];
-    image:  string;
+  name: string;
+  titles: string[];
+  social: Social[];
+  image: string;
 }
 
 export interface Social {
-    name:  string;
-    url:   string;
-    class: string;
+  name: string;
+  url: string;
+  class: string;
 }
 
 export type Skills = Array<SkillCategory>
 
-export interface SkillCategory{
-    name: string;
-    data: Skill[];
+export interface SkillCategory {
+  name: string;
+  data: Skill[];
 }
 
 export interface Skill {
-    name:  string;
-    icon: string;
-    description: string;
-    proficiency: string;
+  name: string;
+  icon: string;
+  description: string;
+  proficiency: string;
 }
 
 export interface LangSpecificData {
-    basic_info: BasicInfo;
-    projects: Project[];
-    skills: Skills;
-    experience: Experience[];
+  basic_info: BasicInfo;
+  projects: Project[];
+  skills: Skills;
+  experience: Experience[];
 }
 
 export interface BasicInfo {
-    description_header: string;
-    description:        string;
-    section_name:       SectionName;
+  description_header: string;
+  description: string;
+  section_name: SectionName;
 }
 
 export interface SectionName {
-    about:      string;
-    projects:   string;
-    skills:     string;
-    experience: string;
-    contact:    string;
+  about: string;
+  projects: string;
+  skills: string;
+  experience: string;
+  contact: string;
 }
 
 export interface Experience {
-    title:        string;
-    logo_path:    string;
-    years:        string;
-    description:  string;
-    technologies: string[];
+  title: string;
+  logo_path: string;
+  years: string;
+  description: string;
+  technologies: string[];
 }
 
 export interface Project {
-    title:        string;
-    startDate:    string;
-    description:  string;
-    images:       string[];
-    url:          string;
-    technologies: Technology[];
+  title: string;
+  startDate: string;
+  description: string;
+  images: string[];
+  url: string;
+  technologies: Technology[];
 }
 
 export interface Technology {
-    class: string;
-    name:  string;
+  class: string;
+  name: string;
 }
-
-
-
-
-
-
-
-
--- src/components/sections/Skill.tsx
+++ src/components/sections/Skill.tsx
@@ -3,23 +3,23 @@ import { Skill as SkillType } from "../loaded_data_types";
 import { Tooltip } from "../utils/Tooltip";
 
 function Skill(props: SkillType) {
-	const [isOpened, setIsOpened] = useState(true);
-	return (
-		<div className="skill" onClick={() => setIsOpened(!isOpened)}>
-			<span className="fancy-heading"></span>
-			<Tooltip text={props.description == '' ? `${props.proficiency}/100` : props.description} cssClass="skill-description">
-				<div className="proficiency-indicator">
-					<div
-						className="bar"
-						style={{ width: `${props.proficiency}%` }}
-					>
-						<span className="name">{props.name}</span>
-						<i className={`${props.icon} skill-icon`}></i>
-					</div>
-				</div>
-			</Tooltip>
-		</div>
-	);
+  const [isOpened, setIsOpened] = useState(true);
+  return (
+    <div className="skill" onClick={() => setIsOpened(!isOpened)}>
+      <span className="fancy-heading"></span>
+      <Tooltip text={props.description == '' ? `${props.proficiency}/100` : props.description} cssClass="skill-description">
+        <div className="proficiency-indicator">
+          <div
+            className="bar"
+            style={{ width: `${props.proficiency}%` }}
+          >
+            <span className="name">{props.name}</span>
+            <i className={`${props.icon} skill-icon`}></i>
+          </div>
+        </div>
+      </Tooltip>
+    </div>
+  );
 }
 
 export default Skill;
